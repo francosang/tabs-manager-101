4a1230a2c0e412332575076ee476493a6324ea95
Franco Sangiacomo <f.sangiacomo@stuart.com>
2023-10-25 12:47:13 +0200

    Fix styles

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,6 @@
 import { Tab } from "../domain/tab";
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -13,54 +12,54 @@ export class MainComponent implements OnInit {
   query?: string;
 
 
-ngOnInit(): void {
-  this.loadTabs();
-}
-
-async loadTabs(): Promise<void> {
-
-  const fetchedTabs = await browser.tabs.query({ currentWindow: true });
-
-  this.tabs = fetchedTabs
-    .map((tab) => (<Tab> {
-      id: tab.id,
-      url: tab.url,
-      name: tab.title,
-      icon: tab.favIconUrl,
-      lastAccessed: tab.lastAccessed,
-    }))
-    .sort((a, b) => b.lastAccessed - a.lastAccessed);
-}
-
- filteredTabs(): Array<Tab> {
-  const query = this.query;
-  if (query != null) {
-    const q = query.toLowerCase();
-    return this.tabs.filter(
-      (t) => t.url.toLowerCase().includes(q) || t.name.toLowerCase().includes(q)
-    );
+  ngOnInit(): void {
+    this.loadTabs();
   }
 
-  return this.tabs;
-}
+  async loadTabs(): Promise<void> {
 
-async selectTab(tab: Tab) {
-  await browser.tabs.update(tab.id, {
-    active: true,
-  });
-  window.close();
-}
+    const fetchedTabs = await browser.tabs.query({ currentWindow: true });
 
-async  removeTab(tab: Tab) {
-  await browser.tabs.remove(tab.id);
+    this.tabs = fetchedTabs
+      .map((tab) => (<Tab> {
+        id: tab.id,
+        url: tab.url,
+        name: tab.title,
+        icon: tab.favIconUrl,
+        lastAccessed: tab.lastAccessed,
+      }))
+      .sort((a, b) => b.lastAccessed - a.lastAccessed);
+  }
 
-  await this.loadTabs();
-}
+  filteredTabs(): Array<Tab> {
+    const query = this.query;
+    if (query != null) {
+      const q = query.toLowerCase();
+      return this.tabs.filter(
+        (t) => t.url.toLowerCase().includes(q) || t.name.toLowerCase().includes(q)
+      );
+    }
 
-async  duplicateTab(tab: Tab) {
-  await browser.tabs.duplicate(tab.id);
+    return this.tabs;
+  }
 
-  await this.loadTabs();
-}
+  async selectTab(tab: Tab) {
+    await browser.tabs.update(tab.id, {
+      active: true,
+    });
+    window.close();
+  }
+
+  async  removeTab(tab: Tab) {
+    await browser.tabs.remove(tab.id);
+
+    await this.loadTabs();
+  }
+
+  async  duplicateTab(tab: Tab) {
+    await browser.tabs.duplicate(tab.id);
+
+    await this.loadTabs();
+  }
 
 }
